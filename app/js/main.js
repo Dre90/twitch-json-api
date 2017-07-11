b5ab224f9f20d1ee21fc9391c082351feb0ff43d
Dre90 <dag.roger.e@gmail.com>
2017-07-11 23:06:01 +0200

    testing

--- app/js/main.js
+++ app/js/main.js
@@ -1,75 +1,110 @@
-var channels = ["blizzheroes","lost_in_house","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var streamer = {name: "", game: "", status: ""};
+// var channels = ["blizzheroes","lost_in_house","ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-(function() {
+// var channels = ["followgrubby", "anderzel", "freecodecamp","brunofin"];
+var channels = ["anderzel", "freecodecamp"];
+var i;
+// var channels = ["brunofin"];
+
 getChannelInfo();
 
-})();
-
-
-
-
 function getChannelInfo() {
-  var i, game, status, logo, name, description, url;
 
   for (i = 0; i < channels.length; i++) {
 
-    getJSON(makeURL("streams", channels[i]), function(err, data) {
-      if (err !== null) {
-        alert('Something went wrong: ' + err);
-      } else {
-
-        if (data.stream === null) {
+    axios.get(makeURL("streams", channels[i]))
+      .then(function (response) {
+        var game, status, name;
+        if (response.data.stream === null) {
           game = "Offline";
           status = "offline";
-        } else if (data.stream === undefined) {
+        } else if (response.data.stream === undefined) {
           game = "Account Closed";
           status = "offline";
         } else {
-          if (data.stream.game === null) {
+          if (response.data.stream.game === null) {
             game = "no game chosen";
           } else {
-            game = data.stream.game + ":";
+            game = response.data.stream.game + ":";
           }
           status = "online";
         }
-      }
-    });//end getJSON function
-    getJSON(makeURL("channels", channels[i]), function(err, data) {
 
-      if (err !== null) {
-        alert('Something went wrong: ' + err);
-      } else {
+console.log("channels[i]", channels[i]);
 
-        // Logo
-        if (data.logo !== null) {
-          logo = data.logo;
+        // name = channels[i];
 
-        } else {
-          logo =  "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+        if (status === "online") {
+          // Name
+          name = response.data.stream.channel.display_name;
+
         }
 
-        // Name
-        if (data.display_name !== null) {
-           name = data.display_name;
-        } else {
-          name = channels[i];
-        }
-
-        // Description
-        if(status === "online") {
-          description = data.status;
-        } else {
-          description = "";
-        }
-
-        // url
-        url = data.url;
-      }
 
 
-      document.getElementById("content-wrapper").innerHTML += '<div><a href="' + url +'" target="_blank">'+ name +' </a> <div><p>' + game + ' ' + description + '</p></div></div> ';
 
-    }); //end getJSON function
+        streamer = {
+          name: name,
+          game: game,
+          status: status
+        };
 
+        // console.log("name ", streamer.name);
+        // console.log("game ", streamer.game);
+
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
+
+      axios.get(makeURL("channels", channels[i]))
+        .then(function (response) {
+
+          var name;
+            // Logo
+            if (response.data.logo !== null) {
+              logo = response.data.logo;
+
+            } else {
+              logo =  "https://dummyimage.com/50x50/ecf0e7/5c5457.jpg&text=0x3F";
+            }
+
+            // Name
+            if (response.data.display_name !== null) {
+               name = response.data.display_name;
+            } else {
+              name = channels[i];
+            }
+
+            // Description
+            if(status === "online") {
+              description = response.data.status;
+            } else {
+              description = "";
+            }
+
+            // url
+            url = response.data.url;
+
+
+
+
+
+          streamer = {
+            name: name
+          };
+
+          // console.log("name ", streamer.name);
+          // console.log("game ", streamer.game);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+
+
+        // console.log("name ", streamer.name);
+        // console.log("game ", streamer.game);
   } // end for loop
 } // end function
